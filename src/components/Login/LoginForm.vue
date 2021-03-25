d09083fda4bbf506f3039f4112421aae63dac654
jfeng5529 <jfeng5529@bths.edu>
2021-03-25 12:34:36 -0400

    signup and login separated

--- src/components/Login/LoginForm.vue
+++ src/components/Login/LoginForm.vue
@@ -1,23 +1,76 @@
 <template>
-  <div>
-    <div><v-btn class="ma-2" @click="student"> Student Login </v-btn></div>
-    <div><v-btn class="ma-2" @click="admin"> Admin Login </v-btn></div>
-  </div>
+  <v-container>
+    <v-alert dark v-if="errorMsg">
+      {{ errorMsg }}
+    </v-alert>
+    <div><v-btn class="ma-2" @click="login"> Login </v-btn></div>
+
+    <v-divider inset></v-divider>
+    <h3 class="my-4">Don't have an account? Sign up here!</h3>
+    <div>
+      <v-btn class="ma-4" @click="login"> Student SignUp </v-btn>
+      <v-btn class="ma-4" @click="admin"> Admin SignUp </v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import firebase from "firebase/app";
+import { auth } from "@/firebase/init";
+import store from "@/store";
 export default {
   name: "LoginForm",
+  computed: {
+    user() {
+      return store.state.user;
+    },
+    adminValidation() {
+      return store.state.adminValidation;
+    }
+  },
   data() {
-    return {};
+    return {
+      errorMsg: null
+    };
   },
   methods: {
-    student() {
-      this.$router.push("/GoogleLogin");
+    async login() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      auth.useDeviceLanguage();
+      auth.signInWithRedirect(provider);
     },
     admin() {
       this.$router.push("/AdminLogin");
     }
+  },
+  mounted() {
+    if (this.user) {
+      this.$router.push("/home");
+    }
+    auth.getRedirectResult().then(async result => {
+      if (result.user != null) {
+        await store.dispatch(
+          "validateAdmin",
+          result.additionalUserInfo.profile.email
+        );
+        if (
+          store.state.adminValidation ||
+          result.additionalUserInfo.profile.hd == "bu.edu"
+        ) {
+          //if this is a BU email or the user is a validated admin
+          if (!this.user) {
+            await store.dispatch("setUser");
+          }
+        } else {
+          //if it is not a BU email
+          this.errorMsg =
+            "You can only sign up for student accounts using BU accounts";
+          setTimeout(() => {
+            store.dispatch("logOut");
+          }, 5000);
+        }
+      }
+    });
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,36 +2,59 @@
   <div>
     Welcome!
 
-    <h1>{{ this.email }}</h1>
-    <!-- <h1 v-if="!this.potentialAdmin">Please wait for verification</h1>
-    <h1 v-if="this.potentialAdmin">Successfully logged in as admin</h1> -->
+    <h1>{{ this.user.email }}</h1>
+    <div v-if="this.isAdmin">
+      <p>Successfully logged in as admin</p>
+      <div>
+        <v-text-field outlined v-model="addAdminEmail"> </v-text-field>
+        <Button @click="addAdmin()"> Admin add admin page </Button>
+      </div>
+    </div>
     <Button @click="signOut"> Log Out</Button>
   </div>
 </template>
 
 <script>
+import { db } from "@/firebase/init";
 export default {
   name: "Home",
   components: {},
-  methods: {
-    async signOut() {
-      await this.$store.dispatch("logOut");
-      this.$router.push("/");
-    }
-  },
   computed: {
     user() {
       return this.$store.state.user;
+    },
+    isAdmin() {
+      return this.$store.state.isAdmin;
     }
   },
   data() {
     return {
-      email: null,
-      potentialAdmin: false
+      addAdminEmail: null
     };
   },
-  async mounted() {
-    this.email = this.user.email;
-  }
+  methods: {
+    async signOut() {
+      await this.$store.dispatch("logOut");
+      this.$router.push("/");
+    },
+    async addAdmin() {
+      if (this.addAdminEmail != null) {
+        alert("Invited: " + this.addAdminEmail);
+        await db
+          .collection("invites")
+          .doc()
+          .set({
+            inviteeEmail: this.addAdminEmail,
+            invitorEmail: this.user.email
+          })
+          .then(() => {
+            // this.$router.push("/");
+          });
+      } else {
+        alert("Please enter a email");
+      }
+    }
+  },
+  async mounted() {}
 };
 </script>
