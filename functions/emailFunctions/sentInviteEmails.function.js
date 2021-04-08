6db398280ff3332c7660dcb269211ab32646de2b
jfeng5529 <jfeng5529@bths.edu>
2021-04-08 13:07:16 -0400

    basic email complete

--- functions/emailFunctions/sentInviteEmails.function.js
+++ functions/emailFunctions/sentInviteEmails.function.js
@@ -1,5 +1,5 @@
 const { google } = require("googleapis");
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 const CLIENT_ID = functions.config().authinfo.id;
 const CLIENT_SECRET = functions.config().authinfo.secret;
@@ -14,37 +14,40 @@ oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 google.options({ auth: oAuth2Client });
 const gmail = google.gmail("v1");
 
-module.exports.sendInviteEmails = functions.https.onCall(async (data, context) => {
-  if (!context.auth) {
-    return { message: "Authentication Required!", code: 401 };
-  }
-  const subject = "🤘 Hello 🤘";
-  const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString("base64")}?=`;
-  const messageParts = [
-    "From: BU Spark <buspark@bu.edu>",
-    "To: Jane Feng <"+ data.email+">",
-    "Content-Type: text/html; charset=utf-8",
-    "MIME-Version: 1.0",
-    `Subject: ${utf8Subject}`,
-    "",
-    "This is a message just to say hello.",
-    "So... <b>Hello!</b>  🤘❤️😎"
-  ];
-  const message = messageParts.join("\n");
-
-  // The body needs to be base64url encoded.
-  const encodedMessage = Buffer.from(message)
-    .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
-
-  const res = await gmail.users.messages.send({
-    userId: "me",
-    requestBody: {
-      raw: encodedMessage
+module.exports.sendInviteEmails = functions.https.onCall(
+  async (data, context) => {
+    if (!context.auth) {
+      return { message: "Authentication Required!", code: 401 };
     }
-  });
-  console.log(res.data);
+    const subject = "🤘 Hello 🤘";
+    const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString(
+      "base64"
+    )}?=`;
+    const messageParts = [
+      "From: BU Spark <buspark@bu.edu>",
+      "To: Jane Feng <" + data.email + ">",
+      "Content-Type: text/html; charset=utf-8",
+      "MIME-Version: 1.0",
+      `Subject: ${utf8Subject}`,
+      "",
+      "This is a message just to say hello.",
+      "So... <b>Hello!</b>  🤘❤️😎"
+    ];
+    const message = messageParts.join("\n");
 
-});
+    // The body needs to be base64url encoded.
+    const encodedMessage = Buffer.from(message)
+      .toString("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+
+    const res = await gmail.users.messages.send({
+      userId: "me",
+      requestBody: {
+        raw: encodedMessage
+      }
+    });
+    console.log(res.data);
+  }
+);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,16 +6,34 @@
     <div v-if="this.isAdmin">
       <p>Successfully logged in as admin</p>
       <div>
-        <v-text-field outlined v-model="addAdminEmail"> </v-text-field>
-        <Button @click="addAdmin()"> Invite Admin </Button>
+        <v-text-field
+          outlined
+          v-model="addAdminEmail"
+          :rules="emailRules"
+          @change="checkEmail"
+        >
+        </v-text-field>
+        <v-btn
+          elevation="2"
+          outlined
+          plain
+          raised
+          class="ma-2"
+          @click="addAdmin()"
+          :disabled="!emailValidated"
+        >
+          Invite Admin
+        </v-btn>
       </div>
     </div>
-    <Button @click="signOut"> Log Out</Button>
+    <v-btn elevation="2" outlined plain raised class="ma-2" @click="signOut">
+      Log Out</v-btn
+    >
   </div>
 </template>
 
 <script>
-import { db } from "@/firebase/init";
+import { functions, db } from "@/firebase/init";
 import store from "@/store";
 // const admin = require("firebase-admin");
 // admin.initializeApp({ projectId: "spark-project-engage" });
@@ -36,13 +54,31 @@ export default {
   },
   data() {
     return {
-      addAdminEmail: null
+      addAdminEmail: null,
+      emailValidated: false,
+      emailRules: [
+        v =>
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            v
+          ) || "Please enter a valid email"
+      ]
     };
   },
   methods: {
     async signOut() {
       await this.$store.dispatch("logOut");
       this.$router.push("/");
+    },
+    checkEmail() {
+      if (
+        !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          this.email
+        )
+      ) {
+        this.emailValidated = true;
+        return;
+      }
+      this.emailValidated = false;
     },
     async addAdmin() {
       if (this.addAdminEmail != null) {
@@ -54,21 +90,11 @@ export default {
             .set({
               inviteeEmail: this.addAdminEmail,
               invitorEmail: this.user.email
-            })
-            .then(() => {});
-
-          // admin.auth().getUserByEmail(this.addAdminEmail).then((user) => {
-          //   console.log("here")
-          //   const currentCustomClaims = user.customClaims;
-          //   if (currentCustomClaims['admin']) {
-          //     return admin.auth().setCustomUserClaims(user.uid, { admin: true });
-          //   }
-          // })
-          // .catch((error) => {
-          //   console.log(error);
-          // });
-
+            });
           alert("Invited: " + this.addAdminEmail);
+          await functions.httpsCallable("sendInviteEmails")({
+            email: this.addAdminEmail
+          });
         } else {
           alert("This email had already been invited to sign up as admin");
         }
