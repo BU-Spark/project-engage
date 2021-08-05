4b5be4eb7b6bd8f6e3fa898fe4289f2ab40643bd
Rishab Nayak <rishab@bu.edu>
2021-08-05 15:01:50 -0400

    merging (#48)
    * Admin manage application form (#45)
    
    * Application form (#47)
    
    * google round button and not-so-working LoginForm horizontal alignment
    
    * sendEmail.function content-type
    
    * LoginForm.vue landing page horizontal alignment
    
    * email content rich text
    
    * Clear editorText field upon submission
    
    * message section align left rather than center
    
    * test pull request fail
    
    * not this issue
    
    * bcc cc
    
    * bcc cc
    
    * email attachments
    
    * link working
    
    * take out print button
    
    * display attachments list and can delete files
    
    * initial application form push
    
    * base application complete
    
    Co-authored-by: Cici Chen <BUicicchen@users.noreply.github.com>
    
    * Added Vue-Formulate
    Co-authored-by: Cici Chen <BUicicchen@users.noreply.github.com>
    
    * schema layout
    
    * combobox plugin outline
    
    * combobox plugin outline
    
    * base application ui, combobox only pass in list not working yet, restructure schema
    
    * plugin UI
    
    * basic firebase set up
    
    * firestore complete for submitting and retrieving
    
    * combobox plugin
    
    * commented out technical skill list
    
    * plugins UI display sample
    
    * add and delete schema item
    
    * application  form display on admin's side
    
    * Admin - Manage Program Application Forms
    
    * back button routing
    
    * cleaned up admin page
    
    * admin add application
    
    * minor
    
    * multiselect, deadline for applications
    
    * multiselect UI
    
    * set up
    
    * data table working
    
    * File upload option (for pdf and image, mainly for resume)
    
    * added edit status/notes function
    
    * application table finished
    
    * Application table phase 1 complete
    
    * minor
    
    * router changes
    
    * v-formulate combobox with input format requirements plugin
    
    * combobox validation, Plugin for textarea (paragraph), Allow admins to add paragraph question type
    
    * merge from master
    
    * merge from master
    
    * made changes to formulate.css
    
    * Added scoped styling that will be apllied across all templates
    
    * Added navbar to the Applicaiton templates
    
    * Added navbar to Admin homepage
    
    Co-authored-by: Cici Chen <iccchen@bu.edu>
    Co-authored-by: rishabnayak <rishab@bu.edu>
    Co-authored-by: jfeng5529 <jfeng5529@bths.edu>
    Co-authored-by: Cici Chen <BUicicchen@users.noreply.github.com>
    
    Co-authored-by: Cici Chen <43624907+BUicicchen@users.noreply.github.com>
    Co-authored-by: siddhantkothari7 <57738104+siddhantkothari7@users.noreply.github.com>
    Co-authored-by: Cici Chen <iccchen@bu.edu>
    Co-authored-by: jfeng5529 <jfeng5529@bths.edu>
    Co-authored-by: Cici Chen <BUicicchen@users.noreply.github.com>

--- src/components/Application/ApplicationForm.vue
+++ src/components/Application/ApplicationForm.vue
@@ -1,5 +1,51 @@
 <template>
   <v-container>
+    <div>
+      <div id="dashboard-container" class="d-flex flex-row mb-6 align-center">
+        <div id="rightSideDashboard">
+          <v-img
+            src="@/assets/sparky.png"
+            max-height="75"
+            max-width="75"
+            class="db-logo"
+          >
+          </v-img>
+          <div id="navigations">
+            <v-btn elevation="0" class="nav-btn" @click="updatePageBody(0)">
+              Dashboard</v-btn
+            >
+            <v-btn elevation="0" class="nav-btn"> Applicants </v-btn>
+            <v-btn elevation="0" class="nav-btn"> Reports </v-btn>
+          </div>
+        </div>
+        <div id="main-actions">
+          <v-btn class="main-action" elevation="0">
+            <v-icon
+              aria-hidden="false"
+              style="color: #36bd90"
+              @click="updatePageBody(2)"
+            >
+              mdi-cog
+            </v-icon>
+          </v-btn>
+          <v-btn class="main-action" elevation="0">
+            <v-icon
+              aria-hidden="false"
+              style="color: #36bd90"
+              @click="updatePageBody(3)"
+            >
+              mdi-bell
+            </v-icon>
+          </v-btn>
+          <v-btn class="main-action" elevation="0" @click="updatePageBody(1)">
+            <v-icon aria-hidden="false" style="color: #36bd90">
+              mdi-account-circle
+            </v-icon>
+            &nbsp; &nbsp; {{ this.user.displayName }}
+          </v-btn>
+        </div>
+      </div>
+    </div>
     <h2>Application Form for {{ this.applicationType }} program</h2>
     <h2>Semester: {{ this.semester }}</h2>
 
@@ -219,5 +265,42 @@ export default {
   margin-bottom: 0;
   color: black;
   background-color: white;
+}
+div#main-actions {
+  margin-right: 25px !important;
+  float: right !important;
+  right: -25px !important;
+  padding: 15px !important;
+  text-align: left !important;
+}
+v-btn {
+  color: #36bd90;
+}
+.main-action {
+  margin-right: 10px !important;
+  border-radius: 15px;
+  padding: 15px 0px;
+}
+div#dashboard-container {
+  background-color: #36bd90;
+  color: black;
+  padding: 15px;
+  width: 100%;
+  justify-content: space-between;
+}
+div#rightSideDashboard {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+}
+.nav-btn {
+  background-color: transparent !important;
+  color: black !important;
+  font-weight: 900 !important;
+  border: none !important;
+  /* font-size: 12px; */
+}
+.db-logo {
+  margin: 5px 25px;
 }
 </style>
