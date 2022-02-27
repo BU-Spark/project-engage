44f15468d8af36c9a1d7506a0dc80f1a6ed2d594
Eric Chao <ericchao@dhcp-wifi-8021x-155-41-110-125.bu.edu>
2022-02-27 16:14:47 -0500

    Fixed time sorting issue of applications

--- src/components/Admin/AdminApplicationDashboard.vue
+++ src/components/Admin/AdminApplicationDashboard.vue
@@ -133,6 +133,7 @@
           item-key="test"
           show-select
           :search="search"
+          :sort="sort"
           class="elevation-1"
         >
           <template v-slot:item.firstname="{ item }">
@@ -189,7 +190,7 @@ export default {
         "team lead",
         "ux designer",
         "frontend developer",
-        "backend developer",
+        "backend developer"
       ],
       position: [],
       programList: [
@@ -198,7 +199,7 @@ export default {
         "Innovation Fellowship | Technical Teammate",
         "Innovation Fellowship | UX Designer",
         "Justice Media Co-Lab",
-        "X-Lab",
+        "X-Lab"
       ],
       program: [],
       statusList: [
@@ -209,18 +210,18 @@ export default {
         "interviewing",
         "accepted",
         "rejcted",
-        "declined",
+        "declined"
       ],
       status: [],
       search: "",
       headers: [
         {
           text: "First Name",
-          value: "firstname",
+          value: "firstname"
         },
         {
           text: "last Name",
-          value: "lastname",
+          value: "lastname"
         },
         // {
         //   text: "Position",
@@ -233,52 +234,73 @@ export default {
         {
           text: "Semester",
           value: "semester",
-          filter: (value) => {
+          filter: value => {
             if (this.chosenSemester.length == 0) return true;
             return this.chosenSemester.includes(value);
-          },
+          }
         },
         {
           text: "Program",
           value: "program",
-          filter: (value) => {
+          filter: value => {
             if (this.program.length == 0) return true;
             return this.program.includes(value);
-          },
+          }
         },
         {
           text: "Year",
-          value: "schoolYear",
+          value: "schoolYear"
         },
         {
           text: "Gender",
-          value: "gender",
+          value: "gender"
         },
         {
           text: "Email",
-          value: "email",
+          value: "email"
         },
         {
           text: "Submisson Time",
           value: "submissionTime",
+          sort: (d1, d2) => {
+            var da1 = new Date(d1);
+            var da2 = new Date(d2);
+            var date1 = Date.UTC(
+              da1.getUTCFullYear(),
+              da1.getUTCMonth(),
+              da1.getUTCDate(),
+              da1.getUTCHours(),
+              da1.getUTCMinutes(),
+              da1.getUTCSeconds()
+            );
+            var date2 = Date.UTC(
+              da2.getUTCFullYear(),
+              da2.getUTCMonth(),
+              da2.getUTCDate(),
+              da2.getUTCHours(),
+              da2.getUTCMinutes(),
+              da2.getUTCSeconds()
+            );
+            return date1 - date2;
+          }
         },
         {
           text: "Status",
           value: "status",
-          filter: (value) => {
+          filter: value => {
             if (this.status.length == 0) return true;
             console.log(this.status);
             if (value) {
               return this.status.includes(value);
             }
-          },
+          }
         },
         {
           text: "Notes",
-          value: "adminNotes",
+          value: "adminNotes"
         },
-        {},
-      ],
+        {}
+      ]
     };
   },
   methods: {
@@ -287,20 +309,20 @@ export default {
     },
     viewProfile(item) {
       let route = this.$router.resolve({
-        name: "studentProfile",
+        name: "studentProfile"
       });
       window.localStorage["params"] = JSON.stringify({
-        item: item,
+        item: item
       });
       window.open(route.href, "_blank");
       this.item = item;
     },
     viewApplication(item) {
       let route = this.$router.resolve({
-        name: "studentApp",
+        name: "studentApp"
       });
       window.localStorage["params"] = JSON.stringify({
-        item: item,
+        item: item
       });
       window.open(route.href, "_blank");
       this.item = item;
@@ -333,29 +355,30 @@ export default {
           .collection(this.editItem.program)
           .doc(this.editItem.uid);
         await application.update({
-          adminNotes: this.editItem.adminNotes,
+          adminNotes: this.editItem.adminNotes
         });
         Object.assign(this.applications[this.editIndex], this.editItem);
       } else if (this.editStatus) {
         if (this.editItem.status) {
           const ref = await db.collection("users").doc(this.editItem.uid);
           let applications = await ref.get();
-          applications =
-            applications.data().applications[this.editItem.semester];
+          applications = applications.data().applications[
+            this.editItem.semester
+          ];
           for (let i = 0; i < applications.length; i++) {
             if (applications[i].type == this.editItem.program) {
               applications[i].status = this.editItem.status;
             }
           }
           await ref.update({
-            applications: applications,
+            applications: applications
           });
           Object.assign(this.applications[this.editIndex], this.editItem);
         }
       }
 
       this.close();
-    },
+    }
   },
   async mounted() {
     const date = new Date();
@@ -377,8 +400,11 @@ export default {
       const profileRef = db.collection("applications").doc("Base");
       for (let type of this.programList) {
         const subCol = await ref.collection(type).get();
-        subCol.forEach(async (element) => {
-          const user = await db.collection("users").doc(element.id).get();
+        subCol.forEach(async element => {
+          const user = await db
+            .collection("users")
+            .doc(element.id)
+            .get();
           const applications = user.data().applications[this.semester[i]];
           let submissionTime;
           let status;
@@ -390,7 +416,7 @@ export default {
                 submissionTime = new Date(
                   submissionTime.seconds * 1000
                 ).toLocaleString("en-US", {
-                  timeZone: "America/New_York",
+                  timeZone: "America/New_York"
                 });
                 status = applications[i].status;
               }
@@ -403,7 +429,7 @@ export default {
           if (profCol.data()) {
             result = {
               ...profCol.data(),
-              ...element.data(),
+              ...element.data()
             };
             result = {
               ...result,
@@ -411,8 +437,8 @@ export default {
                 uid: element.id,
                 semester: this.semester[i],
                 submissionTime: submissionTime,
-                status: status,
-              },
+                status: status
+              }
             };
             this.applications.push(result);
           } else {
@@ -422,14 +448,14 @@ export default {
                 uid: element.id,
                 semester: this.semester[i],
                 submissionTime: submissionTime,
-                status: status,
-              },
+                status: status
+              }
             };
             this.applications.push(result);
           }
         });
       }
     }
-  },
+  }
 };
 </script>
