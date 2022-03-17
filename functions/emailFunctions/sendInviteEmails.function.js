9160692a9b2fb42887d8e3297b78a04bbd49e703
Cici Chen <iccchen@bu.edu>
2022-03-17 18:24:11 -0400

    temp change

--- functions/emailFunctions/sendInviteEmails.function.js
+++ functions/emailFunctions/sendInviteEmails.function.js
@@ -31,8 +31,7 @@ module.exports.sendInviteEmails = functions.https.onCall(
       `Subject: ${utf8Subject}`,
       "",
       "Create your Spark Admin Account by Clicking on the link below!",
-      "So... what are you waiting for? 🤘❤️😎",
-      "<br/> <a href='https://buspark.app/home'> Spark Central Portal </a>"
+      "So... what are you waiting for? 🤘❤️😎"
     ];
     const message = messageParts.join("\n");
 
