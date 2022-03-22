8046c21a8cb3f44e59e121d27b6fc89d270819b1
Cici Chen <iccchen@bu.edu>
2022-03-22 02:38:30 -0400

    merge from master

--- src/components/Admin/InviteAdmin.vue
+++ src/components/Admin/InviteAdmin.vue
@@ -73,7 +73,7 @@ export default {
     checkEmail() {
       if (
         !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-          this.email
+          this.addAdminEmail
         )
       ) {
         this.emailValidated = true;
