188c3b7fed3263f4faddd12927f38510604c7d92
Cici Chen <iccchen@bu.edu>
2022-03-23 11:52:46 -0400

    sendInviteEmails

--- functions/emailFunctions/sendInviteEmails.function.js
+++ functions/emailFunctions/sendInviteEmails.function.js
@@ -1,6 +1,7 @@
+import * as cors from "cors";
+const corsHandler = cors({ origin: true });
 const { google } = require("googleapis");
 const functions = require("firebase-functions");
-const cors = require("cors")({ origin: true });
 
 const CLIENT_ID = functions.config().authinfo.id;
 const CLIENT_SECRET = functions.config().authinfo.secret;
@@ -15,43 +16,41 @@ oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 google.options({ auth: oAuth2Client });
 const gmail = google.gmail("v1");
 
-module.exports.sendInviteEmails = functions.https.onCall(
-  async (data, context) => {
-    cors(data, context, async () => {
-      if (!context.auth) {
-        return { message: "Authentication Required!", code: 401 };
+module.exports.sendInviteEmails = functions.https.onCall((data, context) => {
+  corsHandler(data, context, async () => {
+    if (!context.auth) {
+      return { message: "Authentication Required!", code: 401 };
+    }
+    const subject = "You are Invited to be a Spark Admin!";
+    const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString(
+      "base64"
+    )}?=`;
+    const messageParts = [
+      "From: BU Spark <buspark@bu.edu>",
+      "To: " + data.name + " <" + data.email + ">",
+      "Content-Type: text/html; charset=utf-8",
+      "MIME-Version: 1.0",
+      `Subject: ${utf8Subject}`,
+      "",
+      "Create your Spark Admin Account by Clicking on the link below!",
+      "So... what are you waiting for? 🤘❤️😎",
+      "<br/> <a href='http://localhost:8080/home'> Spark Central Portal </a>"
+    ];
+    const message = messageParts.join("\n");
+
+    // The body needs to be base64url encoded.
+    const encodedMessage = Buffer.from(message)
+      .toString("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+
+    const res = await gmail.users.messages.send({
+      userId: "me",
+      requestBody: {
+        raw: encodedMessage
       }
-      const subject = "You are Invited to be a Spark Admin!";
-      const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString(
-        "base64"
-      )}?=`;
-      const messageParts = [
-        "From: BU Spark <buspark@bu.edu>",
-        "To: " + data.name + " <" + data.email + ">",
-        "Content-Type: text/html; charset=utf-8",
-        "MIME-Version: 1.0",
-        `Subject: ${utf8Subject}`,
-        "",
-        "Create your Spark Admin Account by Clicking on the link below!",
-        "So... what are you waiting for? 🤘❤️😎",
-        "<br/> <a href='http://localhost:8080/home'> Spark Central Portal </a>"
-      ];
-      const message = messageParts.join("\n");
-
-      // The body needs to be base64url encoded.
-      const encodedMessage = Buffer.from(message)
-        .toString("base64")
-        .replace(/\+/g, "-")
-        .replace(/\//g, "_")
-        .replace(/=+$/, "");
-
-      const res = await gmail.users.messages.send({
-        userId: "me",
-        requestBody: {
-          raw: encodedMessage
-        }
-      });
-      console.log(res.data);
     });
-  }
-);
+    console.log(res.data);
+  });
+});
